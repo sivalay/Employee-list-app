810a31b6018a7dee7b73c3b0bb92d15699348493
vonnue-kavya <115480937+vonnue-kavya@users.noreply.github.com>
2025-06-19 15:07:26 +0530

    Revert "feat: Created employee-list component"

--- src/App.vue
+++ src/App.vue
@@ -1,20 +1,3 @@
 <template>
-  <main>
-    <employee-list />
-  </main>
+  <main>Hello</main>
 </template>
-
-<script setup lang="ts">
-import EmployeeList from './components/EmployeeList.vue'
-</script>
-
-<style scoped>
-main {
-  min-height: 100vh;
-  background-color: #7f5a83;
-  background-image: linear-gradient(315deg, #7f5a83 0%, #0d324d 74%);
-  background-repeat: no-repeat;
-  color: #fff;
-  font-size: 18px;
-}
-</style>
